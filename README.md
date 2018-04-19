cc873b17d722ec78d7b7fa67508147921a895929
zhanghongxing <hongxing_zhang@hotmail.com>
2018-04-19 17:59:59 +0800

    Add the folder Vue-js-2-Web-Development-Projects in ivue

---
+++ README.md
@@ -0,0 +1,3 @@
+# ivue
+## Learning Path to Vue
+### Vue.js 2 Web Development Projects
---
+++ Vue-js-2-Web-Development-Projects/chapter2/script.js
@@ -0,0 +1,11 @@
+// New VueJS instance      
+new Vue({
+    // CSS selector of the root DOM element        
+    el: '#notebook',
+    // Some data        
+    data() {
+        return {
+            content: 'This is a note.',
+        }
+    },
+})
