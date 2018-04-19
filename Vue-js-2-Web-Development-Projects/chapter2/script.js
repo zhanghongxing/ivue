b26d514245aec13da32d4c1605a1b397d29f574f
zhanghongxing <hongxing_zhang@hotmail.com>
2018-04-19 19:24:08 +0800

    Project01:Watching changes:Step1

--- Vue-js-2-Web-Development-Projects/chapter2/script.js
+++ Vue-js-2-Web-Development-Projects/chapter2/script.js
@@ -1,18 +1,28 @@
 // New VueJS instance      
 new Vue({
-    // CSS selector of the root DOM element        
-    el: '#notebook',
-    // Some data        
-    data() {
-        return {
-            content: 'This is a note.',
-        }
+  // CSS selector of the root DOM element        
+  el: '#notebook',
+  // Some data        
+  data() {
+    return {
+      content: 'This is a note.',
+    }
+  },
+  // Computed properties
+  computed: {
+    notePreview() { 
+      // Markdown rendered to HTML    
+      return marked(this.content)
     },
-    // Computed properties
-    computed: {
-      notePreview() { 
-        // Markdown rendered to HTML    
-        return marked(this.content)
+  },
+  
+  // Change watchers  
+  watch: {
+    // Watching 'content' data property    
+    content: {
+      handler(val, oldVal) {
+        console.log('new note:', val, 'old note:', oldVal)
       },
     },
+  },
 })
