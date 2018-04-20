400fb423db411578d49b4b664a90d9e3da583187
zhanghongxing <hongxing_zhang@hotmail.com>
2018-04-20 17:39:09 +0800

    Project01:The current note

--- Vue-js-2-Web-Development-Projects/chapter2/script.js
+++ Vue-js-2-Web-Development-Projects/chapter2/script.js
@@ -5,7 +5,6 @@ new Vue({
   // Some data        
   data() {
     return {
-      content: localStorage.getItem('content') || 'You can write in **markdown**',
       // New! A note array
       notes: [],
       // Id of the selected note
@@ -16,21 +15,19 @@ new Vue({
   computed: {
     notePreview() { 
       // Markdown rendered to HTML    
-      return marked(this.content)
+      return this.selectedNote ? marked(this.selectedNote.content) : ''
     },
     addButtonTitle() {
       return this.notes.length + ' note(s) already'
+    },
+    selectedNote() {
+      // We return the matching note with selectedId          
+      return this.notes.find(note => note.id === this.selectedId)
     },
   },
   
   // Change watchers  
   watch: {
-    // Watching 'content' data property    
-    content (val, oldVal) {
-      console.log('new note:', val, 'old note:', oldVal),
-      console.log('saving note:', this.content)
-      localStorage.setItem('content', this.content)
-    },
   },
   
   methods: {
@@ -38,9 +35,6 @@ new Vue({
       console.log('saving note:', this.content)
       localStorage.setItem('content', this.content)
       this.reportOperation('saving')
-    },
-    reportOperation (opName) {
-      console.log('The', opName, 'operation was completed!')
     },
     // Add a note with some default content and select it  
     addNote() {
@@ -71,8 +65,8 @@ new Vue({
   created() { 
     // Set the content to the stored value    
     // or to a default string if nothing was saved    
-    this.content = localStorage.getItem('content') || 'You can write in **markdown**'
+    // this.content = localStorage.getItem('content') || 'You can write in **markdown**'
   },
 })
 
-console.log('restored note:', localStorage.getItem('content'))
+// console.log('restored note:', localStorage.getItem('content'))
