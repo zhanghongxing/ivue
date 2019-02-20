4983224a59495e519e08889918184e756b2879f8
zhanghongxing <hongxing_zhang@hotmail.com>
2019-02-20 14:10:08 +0800

    advanced-setup:06-template

--- Vue-js-2-Web-Development-Projects/advanced-setup/demo/src/Test.vue
+++ Vue-js-2-Web-Development-Projects/advanced-setup/demo/src/Test.vue
@@ -2,13 +2,21 @@
   <div>
     <p>{{ message }}</p>
     <input v-model="message">
+    <ul class="movies">
+      <li v-for="movie of movies" class="movie">{{ movie.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return { message: "Hello world" };
+    return { message: "Hello world",
+             movies: [
+                { title: 'Star Wars' },
+                { title: 'Blade Runner' },
+              ],
+    };
   }
 };
 </script>
