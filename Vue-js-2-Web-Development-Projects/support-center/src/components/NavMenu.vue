8156ae82b83e3e61514bdf5f945fb3d08a447e3c
zhanghongxing <hongxing_zhang@hotmail.com>
2019-02-20 22:28:53 +0800

    project03:06-active-class

--- Vue-js-2-Web-Development-Projects/support-center/src/components/NavMenu.vue
+++ Vue-js-2-Web-Development-Projects/support-center/src/components/NavMenu.vue
@@ -1,7 +1,15 @@
 <template>
   <nav class="menu">
     <!-- Links here -->
-    <router-link :to="{ name: 'home' }">Home</router-link>
+    <router-link :to="{ name: 'home' }" exact>Home</router-link>
     <router-link :to="{ name: 'faq' }">FAQ</router-link>
   </nav>
 </template>
+
+<style lang="stylus" scoped>
+@import '../style/imports';
+
+.router-link-active {
+  border-bottom-color: $primary-color;
+}
+</style>
