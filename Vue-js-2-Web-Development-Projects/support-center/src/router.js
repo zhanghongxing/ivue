27c90c63cd77cd1cc7e36205aa29f0fb71c2d528
zhanghongxing <hongxing_zhang@hotmail.com>
2019-02-20 21:40:00 +0800

    project03:04-router-object

--- Vue-js-2-Web-Development-Projects/support-center/src/router.js
+++ Vue-js-2-Web-Development-Projects/support-center/src/router.js
@@ -1,4 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from './components/Home.vue'
+import FAQ from './components/FAQ.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: Home }, 
+  { path: '/faq', name: 'faq', component: FAQ },
+]
+
+const router = new VueRouter({
+  routes,
+})
+
+export default router
 
 Vue.use(VueRouter)
