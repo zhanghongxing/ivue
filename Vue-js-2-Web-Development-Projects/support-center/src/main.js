2d5e921a64ed3bec69757807956f66c6413cd387
zhanghongxing <hongxing_zhang@hotmail.com>
2019-02-20 20:53:26 +0800

    project03:03-Layout-with-routerview

--- Vue-js-2-Web-Development-Projects/support-center/src/main.js
+++ Vue-js-2-Web-Development-Projects/support-center/src/main.js
@@ -1,7 +1,9 @@
 import 'babel-polyfill'
 import Vue from 'vue'
+import AppLayout from './components/AppLayout.vue'
 
 new Vue({
   el: '#app',
-  render: h => h('div', 'Support center'),
+  //render: h => h('div', 'Support center'),
+  render: h => h(AppLayout),
 })
