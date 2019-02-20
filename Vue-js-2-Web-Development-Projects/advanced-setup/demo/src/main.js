948b969dffbc3050d63dc68bb14221bc78d483ca
zhanghongxing <hongxing_zhang@hotmail.com>
2019-02-20 17:23:22 +0800

    advanced-setup:11-Components inside components

--- Vue-js-2-Web-Development-Projects/advanced-setup/demo/src/main.js
+++ Vue-js-2-Web-Development-Projects/advanced-setup/demo/src/main.js
@@ -1,9 +1,11 @@
 import 'babel-polyfill'
 import Vue from 'vue'
-import Test from './Test.vue'
+//import Test from './Test.vue'
+import Movies from './Movies.vue'
 
 new Vue({
   el: '#app',
   //render: h => h('div', 'hello world'),
-  ...Test,
+  //...Test,
+  ...Movies,
 })
