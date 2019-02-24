0e5ac961c2a85475ee3bee273cbc06aebe5fc859
zhanghongxing <hongxing_zhang@hotmail.com>
2019-02-24 22:16:28 +0800

    consuming-api:06-fetch-method

--- Vue-js-2-Web-Development-Projects/consuming-api/client/src/components/FAQ.vue
+++ Vue-js-2-Web-Development-Projects/consuming-api/client/src/components/FAQ.vue
@@ -26,17 +26,16 @@ export default {
 
   async created () {
     this.loading = true
+    
     try {
-      const response = await fetch('http://localhost:3000/questions')
-      if (response.ok) {
-        this.questions = await response.json()
-      } else {
-        throw new Error('error')
-      }
+      this.questions = await this.$fetch('questions')
     } catch (e) {
       this.error = e
     }
+
     this.loading = false
   },
+
 };
+
 </script>
--- Vue-js-2-Web-Development-Projects/consuming-api/client/src/plugins/fetch.js
+++ Vue-js-2-Web-Development-Projects/consuming-api/client/src/plugins/fetch.js
@@ -3,8 +3,20 @@ let baseUrl
 export default {
   install(Vue, options) {
     console.log('fetch plugin is installed!', options)
-    
+
     baseUrl = options.baseUrl
 
-  } 
+    Vue.prototype.$fetch = $fetch
+  },
+}
+
+export async function $fetch (url) {
+  const response = await fetch(`${baseUrl}${url}`)
+  if (response.ok) {
+    const data = await response.json()
+    return data
+  } else {
+    const error = new Error('error')
+    throw error
+  }
 }
